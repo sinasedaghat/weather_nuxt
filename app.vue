0c6110a11a006ab71eadf97c9594ed40298925f9
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-03-25 16:40:47 +0330

    create layout and pages directory and add file in pages for router configes and crete default layout

--- app.vue
+++ app.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-  const testStore = useTestStore()
 
-  const changeVal = () => {
-    testStore.testFunc()
-  }
 
 </script>
 
 <template>
-  {{ testStore.testValue }}
-  <h3 @click="changeVal">change</h3>
-  <v-chip color="primary">
-    Primary
-  </v-chip>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
+
+  const route = useRoute()
+  const router = useRouter()
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar color="primary">
+      <!-- title -->
+      <v-row class="ma-0 pa-0 ms-6" align="center" justify="center">
+        <v-col class="ma-0 pa-0 mb-n3" cols="12">
+          <h1 class="text-info" style="font-family: Brush Script MT, cursive;">WeatherWise</h1>
+        </v-col>
+        <v-col class="ma-0 pa-0" cols="12">
+          <small class="text-info" style="font-family: Brush Script MT, cursive; font-size: x-small;">Weather & Pollution Report</small>
+        </v-col>
+      </v-row>
+      <v-spacer />
+      <!-- favorite route -->
+      <!-- :disabled="!hasFavorite"  -->
+      <v-btn
+        v-if="route.name !== 'favorites'"
+        icon
+        @click="router.push({ name: 'favorites' })"
+      >
+        <v-icon>mdi-heart</v-icon>
+        <v-tooltip
+          activator="parent"
+          location="start"
+        >
+          <span class="text-caption" v-text="'Favorite Cities'" />
+        </v-tooltip>
+      </v-btn>
+      <!-- home route -->
+      <v-btn
+        v-if="route.name !== 'index'"
+        icon
+        @click="router.push({ name: 'index' })"
+      >
+        <v-icon>mdi-home</v-icon>
+        <v-tooltip
+          activator="parent"
+          location="start"
+        >
+          <span class="text-caption" v-text="'Go To Home'" />
+        </v-tooltip>
+      </v-btn>
+    </v-app-bar>
+    <!-- main -->
+    <v-main>
+      <slot />
+    </v-main>
+</v-layout>
+</template>
